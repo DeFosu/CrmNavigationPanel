62505d401cd17f0249607ad1b6d06944282c7cc7
Yarik <defosu.33@gmail.com>
2024-10-18 10:01:11 +0300

    feat: Create Logo.tsx component

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,10 +1,10 @@
+import { DiReact } from "react-icons/di";
 import React from "react";
-import reactLogo from "../../assets/react.svg";
 
 const Logo: React.FC = () => {
     return (
-        <div className="w-10 h-10">
-            <img src={reactLogo} alt="" className="object-cove aspect-square" />
+        <div>
+            <DiReact size={40} />
         </div>
     );
 };
