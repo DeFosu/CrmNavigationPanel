21018e0e4cc1a5da006a3cebb406b2dc278166f3
Yarik <defosu.33@gmail.com>
2024-10-19 09:16:43 +0300

    fix: Change the width of the component

--- src/components/CrmNavigationPanel/CrmNavigationPanel.tsx
+++ src/components/CrmNavigationPanel/CrmNavigationPanel.tsx
@@ -1,16 +1,18 @@
 import { CgShoppingBag } from "react-icons/cg";
 import { BiLinkExternal } from "react-icons/bi";
 import { FiLifeBuoy } from "react-icons/fi";
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../Logo/Logo";
 import Avatar from "../Avatar/Avatar";
 import NavigationItemIcon from "./segments/NavigationItemIcon/NavigationItemIcon";
 import NavigationItem from "./segments/NavigationItem/NavigationItem";
 
 const CrmNavigationPanel: React.FC = () => {
+    const [navigationStoreOpen, setNavigationStoreOpen] = useState(false);
+
     return (
-        <div className="flex w-full max-w-[400px]">
-            <div className="bg-slate-300/40 px-4 py-6 rounded-l-3xl rounded-r-sm flex flex-col items-center justify-between shadow-md">
+        <div className="flex w-full max-w-[360px]">
+            <div className="flex flex-col items-center justify-between px-4 py-6 rounded-r-sm shadow-md bg-slate-300/40 rounded-l-3xl">
                 <div className="flex flex-col items-center gap-4">
                     <Logo />
                     <div className="flex flex-col items-center gap-0.5">
@@ -31,7 +33,7 @@ const CrmNavigationPanel: React.FC = () => {
                 </div>
             </div>
             <div className="pt-8 px-4 pb-4 flex flex-col shadow w-full min-w-[200px] gap-4">
-                <div className="flex w-full items-center">
+                <div className="flex items-center w-full">
                     <div className="w-full text-lg font-semibold line-clamp-1">
                         Site Title
                     </div>
@@ -39,7 +41,22 @@ const CrmNavigationPanel: React.FC = () => {
                 </div>
                 <div className="w-full border rounded-full" />
                 <div>
-                    <NavigationItem isActive>
+                    <div
+                        onClick={() =>
+                            setNavigationStoreOpen(!navigationStoreOpen)
+                        }
+                        className="cursor-pointer"
+                    >
+                        <NavigationItem isActive={navigationStoreOpen}>
+                            <CgShoppingBag size={20} />
+                            Store
+                        </NavigationItem>
+                    </div>
+                    <NavigationItem>
+                        <CgShoppingBag size={20} />
+                        Store
+                    </NavigationItem>
+                    <NavigationItem>
                         <CgShoppingBag size={20} />
                         Store
                     </NavigationItem>
--- src/components/CrmNavigationPanel/segments/NavigationItem/NavigationItem.tsx
+++ src/components/CrmNavigationPanel/segments/NavigationItem/NavigationItem.tsx
@@ -1,3 +1,4 @@
+import { AiOutlineMinus } from "react-icons/ai";
 import React from "react";
 import { AiOutlinePlus } from "react-icons/ai";
 import type { NavigationItemProps } from "./NavigationItemProps";
@@ -13,36 +14,44 @@ const NavigationItem: React.FC<NavigationItemProps> = ({
                     isActive ? "bg-slate-200/60" : "bg-transparent"
                 }`}
             >
-                <div className="flex justify-start items-center gap-2 text-base leading-none font-medium">
+                <div className="flex items-center justify-start gap-2 text-base font-medium leading-none">
                     {children}
                 </div>
                 <div className="text-slate-950/40">
-                    <AiOutlinePlus size={18} />
+                    {isActive ? (
+                        <AiOutlineMinus size={18} />
+                    ) : (
+                        <AiOutlinePlus size={18} />
+                    )}
                 </div>
             </div>
-            <div className="relative mt-2">
-                <div className="absolute h-full left-4 top-0 py-4 z-20">
+            <div
+                className={`relative mt-1 transition-all h-fit duration-300 ease-in-out ${
+                    !isActive && "hidden"
+                }`}
+            >
+                <div className="absolute top-0 z-20 h-full py-4 left-4">
                     <div className="h-full w-0.5 bg-gray-300 rounded-full" />
                 </div>
                 <div className="flex flex-col gap-0.5">
                     <div className="group hover:bg-slate-200/60 text-slate-500 hover:text-slate-950 px-2 pl-[0.812rem]  py-1.5 rounded flex gap-4 items-center transition-colors duration-100 ease-in-out">
-                        <div className="z-20 w-2 h-fit aspect-square rounded-full group-hover:bg-slate-950 transition-colors duration-100 ease-in-out" />
+                        <div className="z-20 w-2 transition-colors duration-100 ease-in-out rounded-full h-fit aspect-square group-hover:bg-slate-950" />
                         Item-1
                     </div>
                     <div className="group hover:bg-slate-200/60 text-slate-500 hover:text-slate-950 px-2 pl-[0.812rem]  py-1.5 rounded flex gap-4 items-center transition-colors duration-100 ease-in-out">
-                        <div className="z-20 w-2 h-fit aspect-square rounded-full group-hover:bg-slate-950 transition-colors duration-100 ease-in-out" />
+                        <div className="z-20 w-2 transition-colors duration-100 ease-in-out rounded-full h-fit aspect-square group-hover:bg-slate-950" />
                         Item-1
                     </div>
                     <div className="group hover:bg-slate-200/60 text-slate-500 hover:text-slate-950 px-2 pl-[0.812rem]  py-1.5 rounded flex gap-4 items-center transition-colors duration-100 ease-in-out">
-                        <div className="z-20 w-2 h-fit aspect-square rounded-full group-hover:bg-slate-950 transition-colors duration-100 ease-in-out" />
+                        <div className="z-20 w-2 transition-colors duration-100 ease-in-out rounded-full h-fit aspect-square group-hover:bg-slate-950" />
                         Item-1
                     </div>
                     <div className="group hover:bg-slate-200/60 text-slate-500 hover:text-slate-950 px-2 pl-[0.812rem]  py-1.5 rounded flex gap-4 items-center transition-colors duration-100 ease-in-out">
-                        <div className="z-20 w-2 h-fit aspect-square rounded-full group-hover:bg-slate-950 transition-colors duration-100 ease-in-out" />
+                        <div className="z-20 w-2 transition-colors duration-100 ease-in-out rounded-full h-fit aspect-square group-hover:bg-slate-950" />
                         Item-1
                     </div>
                     <div className="group hover:bg-slate-200/60 text-slate-500 hover:text-slate-950 px-2 pl-[0.812rem]  py-1.5 rounded flex gap-4 items-center transition-colors duration-100 ease-in-out">
-                        <div className="z-20 w-2 h-fit aspect-square rounded-full group-hover:bg-slate-950 transition-colors duration-100 ease-in-out" />
+                        <div className="z-20 w-2 transition-colors duration-100 ease-in-out rounded-full h-fit aspect-square group-hover:bg-slate-950" />
                         Item-1
                     </div>
                 </div>
